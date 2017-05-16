a7dc9400cba57c1299b888d5c37a6cf77122a19b
Camille <camille.dresse@gmail.com>
2017-05-16 11:42:48 +0200

    Update arimaa.pl

--- ArimaaIA02/apps/arimaa/arimaa.pl
+++ ArimaaIA02/apps/arimaa/arimaa.pl
@@ -1,7 +1,42 @@
 :- module(bot,
       [  get_moves/3
       ]).
-	
+%Regarde si un element fait parti de la liste	
+element(X, [X|Q]).
+element(X, [T|Q]):- element (X,Q).
+
+%Concatene deux liste dans une troisieme
+concat([],L,L).
+concat([T|Q],L,[T|R]):- concat(Q,L,R).
+
+%indique si une case n'est pas vide (1 si oui, 0 sinon)
+est_pas_vide([X,Y],Board,1):-element([X,Y,_,_],Board).
+est_pas_vide([X,Y],Board,0):-\+element([X,Y,_,_],Board).
+
+%indique la force d'une piece
+force([_,_,rabbit,_],0).
+force([_,_,cat,_],1).
+force([_,_,dog,_],2).
+force([_,_,horse,_],3).
+force([_,_,camel,_],4).
+force([_,_,elephant,_],5).
+
+%indique si deux pièces sont amies (1 si oui 0 sinon)
+ami([_,_,_,X],[_,_,_,X],1).
+ami([_,_,_,X],[_,_,_,Y],0):-X\=Y.
+
+%Donne les quatres cases adjacentes de la piece
+adjacent([X,Y,_,_],[X+1,Y],[X,Y+1]):- X=0,Y=0,!.
+adjacent([X,Y,_,_],[X+1,Y],[X,Y-1]):- X=0,Y=8,!.
+adjacent([X,Y,_,_],[X-1,Y],[X,Y+1]):- X=8,Y=0,!.
+adjacent([X,Y,_,_],[X-1,Y],[X,Y-1]):- X=8,Y=8,!.
+adjacent([X,Y,_,_],[X+1,Y],[X,Y+1],[X,Y-1]):- X=0,!.
+adjacent([X,Y,_,_],[X-1,Y],[X,Y+1],[X,Y-1]):- X=8,!.
+adjacent([X,Y,_,_],[X+1,Y],[X-1,Y],[X,Y+1]):- Y=0,!.
+adjacent([X,Y,_,_],[X+1,Y],[X-1,Y],[X,Y-1]):- Y=8,!.
+adjacent([X,Y,_,_],[X+1,Y],[X-1,Y],[X,Y+1],[X,Y-1]).
+
+
 % A few comments but all is explained in README of github
 
 % get_moves signature
